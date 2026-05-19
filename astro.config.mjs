9b81531cff6885c9fc7dda9557e2661b3af2892f
van360sound <ruvedia@hotmail.com>
2026-05-19 11:15:30 +0200

    Revert Vercel adapter to fix 404 Output Directory issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,9 @@ import { defineConfig } from 'astro/config';
 
 
 import sitemap from '@astrojs/sitemap';
-import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: vercel(),
   site: 'https://instalaciones-sirga.es',
   output: 'static',
   vite: {
